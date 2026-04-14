ad27087d32944a95f7c69ecf9743035d8d542e77
Davi <davibirds@gmail.com>
2026-04-13 22:18:14 -0300

    Fixed hardcoded path problem

--- sw.js
+++ sw.js
@@ -1,3 +1,24 @@
+let currentFolder = null
+let currentQuest = null
+
+self.addEventListener('message', event => {
+    currentFolder = event.data.folder
+    currentQuest = event.data.quest
+})
+
 self.addEventListener('fetch', event => {
-    console.log(event.request.url)
+    if(event.request.url.includes("/common/") || event.request.url.includes("/lang/") || event.request.url.includes("/global/")) {
+        const url = event.request.url
+        let newUrl = url
+        if (url.includes("/common/"))
+            newUrl = url.replace('/common/', `/swf/${currentFolder}/quest/${currentQuest}/common/`)
+        else if (url.includes("/lang/"))
+            newUrl = url.replace('/lang/', `/swf/${currentFolder}/quest/${currentQuest}/lang/`)
+        else if (url.includes("/global/"))
+            newUrl = url.replace('/global/', `/swf/${currentFolder}/global/`)
+        event.respondWith(fetch(newUrl))
+    }
+    else {
+        event.respondWith(fetch(event.request))
+    }
 })
