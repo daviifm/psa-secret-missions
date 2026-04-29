031a208cc61c384c748725c8fcd9d610a2f6c9e0
Davi <davibirds@gmail.com>
2026-04-29 20:02:29 -0300

    Adding new path to url replace

--- sw.js
+++ sw.js
@@ -14,12 +14,13 @@ self.addEventListener('fetch', event => {
     if(event.request.url.includes("/common/") || event.request.url.includes("/lang/") || event.request.url.includes("/global/")) {
         const url = event.request.url
         let newUrl = url
+        const basePath = self.location.pathname.replace('sw.js', '')
         if (url.includes("/common/"))
-            newUrl = url.replace('/common/', `/swf/${currentFolder}/quest/${currentQuest}/common/`)
+            newUrl = url.replace(/.*\/common\//, `${self.location.origin}${basePath}swf/${currentFolder}/quest/${currentQuest}/common/`)
         else if (url.includes("/lang/"))
-            newUrl = url.replace('/lang/', `/swf/${currentFolder}/quest/${currentQuest}/lang/`)
+            newUrl = url.replace(/.*\/lang\//, `${self.location.origin}${basePath}swf/${currentFolder}/quest/${currentQuest}/lang/`)
         else if (url.includes("/global/"))
-            newUrl = url.replace('/global/', `/swf/${currentFolder}/global/`)
+            newUrl = url.replace(/.*\/global\//, `${self.location.origin}${basePath}swf/${currentFolder}/global/`)
         event.respondWith(fetch(newUrl))
     }
     else {
