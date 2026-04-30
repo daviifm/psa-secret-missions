3ce258171db1273819e6e5b8cc27989e5513c376
Davi <davibirds@gmail.com>
2026-04-29 21:11:47 -0300

    Adding debug for service worker error

--- sw.js
+++ sw.js
@@ -24,3 +24,7 @@ self.addEventListener('fetch', event => {
         event.respondWith(fetch(event.request))
     }
 })
+
+self.addEventListener('activate', event => {
+    event.waitUntil(clients.claim())
+})
