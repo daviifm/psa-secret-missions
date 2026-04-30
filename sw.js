8c906ca1d18e37f53e9fb98535884092e11d48bf
Davi <davibirds@gmail.com>
2026-04-29 21:17:45 -0300

    Fixed service worker only loading after refresh

--- sw.js
+++ sw.js
@@ -8,6 +8,7 @@ self.addEventListener('message', event => {
 })
 
 self.addEventListener('fetch', event => {
+    
     if(event.request.url.includes("/common/") || event.request.url.includes("/lang/") || event.request.url.includes("/global/")) {
         const url = event.request.url
         let newUrl = url
