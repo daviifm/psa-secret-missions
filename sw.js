a860061269c17c1eb64ec716675cc38dc7f73447
Davi <davibirds@gmail.com>
2026-05-10 17:57:13 -0300

    Added debug for infinite loading problem

--- sw.js
+++ sw.js
@@ -7,8 +7,11 @@ self.addEventListener('activate', event => {
 })
 
 self.addEventListener('message', event => {
+    console.log('Message:', event.data)
     currentFolder = event.data.folder
     currentQuest = event.data.quest
+    console.log('currentFolder:', currentFolder)
+    console.log('currentQuest:', currentQuest)
 })
 
 self.addEventListener('fetch', event => {
@@ -18,6 +21,7 @@ self.addEventListener('fetch', event => {
         const basePath = self.location.pathname.replace('sw.js', '')
         const folder = currentFolder || urlParams.get('folder')
         const quest = currentQuest || urlParams.get('quest')
+        console.log('folder:', folder, 'quest:', quest)
         if (url.includes("/common/"))
             newUrl = url.replace(/.*\/common\//, `${self.location.origin}${basePath}swf/${folder}/quest/${quest}/common/`)
         else if (url.includes("/lang/"))
