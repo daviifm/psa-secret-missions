e4a91fab2bda7d3a93f1d9afa84759463d6bca04
Davi <davibirds@gmail.com>
2026-05-12 08:59:48 -0300

    Fixed multiple SWs running at the same time

--- sw.js
+++ sw.js
@@ -1,36 +1,48 @@
 const urlParams = new URLSearchParams(self.location.search)
 let currentFolder = urlParams.get('folder')
 let currentQuest = urlParams.get('quest')
+const missionMap = new Map()
+
+self.addEventListener('install', event => {
+    self.skipWaiting()
+})
 
 self.addEventListener('activate', event => {
     event.waitUntil(clients.claim())
 })
 
 self.addEventListener('message', event => {
-    console.log('Message:', event.data)
+    const clientId = event.source.id
+    missionMap.set(clientId, {
+        folder: event.data.folder,
+        quest: event.data.quest
+    })
     currentFolder = event.data.folder
     currentQuest = event.data.quest
-    console.log('currentFolder:', currentFolder)
-    console.log('currentQuest:', currentQuest)
 })
 
 self.addEventListener('fetch', event => {
     if(event.request.url.includes("/common/") || event.request.url.includes("/lang/") || event.request.url.includes("/global/")) {
-        const url = event.request.url
-        let newUrl = url
-        const basePath = self.location.pathname.replace('sw.js', '')
-        const folder = currentFolder || urlParams.get('folder')
-        const quest = currentQuest || urlParams.get('quest')
-        console.log('folder:', folder, 'quest:', quest)
-        if (url.includes("/common/"))
-            newUrl = url.replace(/.*\/common\//, `${self.location.origin}${basePath}swf/${folder}/quest/${quest}/common/`)
-        else if (url.includes("/lang/"))
-            newUrl = url.replace(/.*\/lang\//, `${self.location.origin}${basePath}swf/${folder}/quest/${quest}/lang/`)
-        else if (url.includes("/global/"))
-            newUrl = url.replace(/.*\/global\//, `${self.location.origin}${basePath}swf/${folder}/global/`)
-        event.respondWith(fetch(newUrl))
-    }
-    else {
+        event.respondWith(
+            clients.get(event.clientId).then(client => {
+                const mission = missionMap.get(event.clientId) ||
+                                { folder: currentFolder, quest: currentQuest } ||
+                                { folder: urlParams.get('folder'), quest: urlParams.get('quest') }
+                const folder = mission.folder
+                const quest = mission.quest
+                const url = event.request.url
+                const basePath = self.location.pathname.replace('sw.js', '')
+                let newUrl = url
+                if (url.includes("/common/"))
+                    newUrl = url.replace(/.*\/common\//, `${self.location.origin}${basePath}swf/${folder}/quest/${quest}/common/`)
+                else if (url.includes("/lang/"))
+                    newUrl = url.replace(/.*\/lang\//, `${self.location.origin}${basePath}swf/${folder}/quest/${quest}/lang/`)
+                else if (url.includes("/global/"))
+                    newUrl = url.replace(/.*\/global\//, `${self.location.origin}${basePath}swf/${folder}/global/`)
+                return fetch(newUrl)
+            })
+        )
+    } else {
         event.respondWith(fetch(event.request))
     }
 })
